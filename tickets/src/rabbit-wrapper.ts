742a55373f9b0c57dc718f6e7814e134874633e7
azizbenayed <101248995+azizbenayed@users.noreply.github.com>
2026-03-02 12:08:05 +0100

    Update RabbitWrapper to use amqp for connection
    Refactor RabbitWrapper to use 'amqp' instead of 'client'.

--- tickets/src/rabbit-wrapper.ts
+++ tickets/src/rabbit-wrapper.ts
@@ -1,18 +1,24 @@
-import client, { Channel, ChannelModel } from "amqplib";
+import amqp, { Channel } from "amqplib";
 
 class RabbitWrapper {
+
   private _channel?: Channel;
-  connection!: ChannelModel; //Connection;
+  connection: any;
 
   get client() {
     if (!this._channel) {
       throw new Error("RABBIT connection needs to be initialized first!");
     }
+
     return this._channel;
   }
+
   async connect(url: string) {
+
     if (!this.connection) {
-      this.connection = await client.connect(url, "heartbeat=40");
+
+      this.connection = await amqp.connect(url);
+
       this._channel = await this.connection.createChannel();
     }
   }
