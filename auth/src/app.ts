6eff8f32de6081ce9eeee55a49a270a0b9608f5b
azizbenayed <101248995+azizbenayed@users.noreply.github.com>
2026-05-04 11:52:31 +0100

    Replace cookie-parser with cookie-session middleware

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-import cookieParser from "cookie-parser";
+import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@eftickets/common";
 
 import { currentUserRouter } from "./routes/current-user";
@@ -10,16 +10,23 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
+
 app.set("trust proxy", true);
 app.use(json());
-app.use(cookieParser());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async (req, res) => {
+app.all("*", async () => {
   throw new NotFoundError();
 });
 
