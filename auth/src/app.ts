5f84991236563fc550a1a0f0fc94abd50c840f78
azizbenayed <101248995+azizbenayed@users.noreply.github.com>
2026-05-04 11:58:29 +0100

    Update app.ts

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-import cookieSession from "cookie-session";
+import cookieParser from "cookie-parser";
 import { errorHandler, NotFoundError } from "@eftickets/common";
 
 import { currentUserRouter } from "./routes/current-user";
@@ -10,23 +10,16 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
-
 app.set("trust proxy", true);
 app.use(json());
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
-);
+app.use(cookieParser());
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async () => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
