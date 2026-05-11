ac1e4235af028f1458cc91601a914641839de5b0
azizbenayed <101248995+azizbenayed@users.noreply.github.com>
2026-05-11 21:34:51 +0100

    Implement token response in signup route
    Refactor signup route to send JWT token in response.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { validateRequest, BadRequestError } from "@eftickets/common";
 
 import { User } from "../models/user";
+import { UserDocMethod } from "../types/IUser";
 
 const router = express.Router();
 
@@ -28,8 +29,35 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
-    res.status(201).send(user);
+    await sendTokenResponse(user as any, 201, res);
   }
 );
+
+interface TokenOptions {
+  maxAge: number;
+  httpOnly: boolean;
+  secure: boolean;
+  sameSite: "lax" | "strict" | "none";
+}
+
+const sendTokenResponse = async (
+  user: UserDocMethod,
+  codeStatus: number,
+  res: Response
+) => {
+  const token = await user.getJwtToken();
+
+  const options: TokenOptions = {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  };
+
+  res.status(codeStatus).cookie("session", token, options).send({
+    id: user.id,
+    email: user.email,
+  });
+};
 
 export { router as signupRouter };
